fbc0594e148c50f61460a32ed8456694c5b31332
Swaraj Anand <swrjnnd@gmail.com>
2024-05-04 21:10:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Profile from './components/Profile'
+import Work from './components/Work'
+import Skill from './components/Skill'
+import Icon from './components/Icon'
+
+function App() {
+  return (
+    <div className='scale-90'>
+      <Profile/>
+      <Work/>
+      <Skill/>
+      <Icon/>
+    </div>
+  )
+}
+
+export default App
